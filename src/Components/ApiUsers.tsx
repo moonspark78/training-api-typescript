69cd79392d5ad6cbdec956065f21870ba8332713
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-01-30 15:41:25 +0100

    :rocket: :tada: :rocket: display data  :tada:

--- src/Components/ApiUsers.tsx
+++ src/Components/ApiUsers.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import "./ApiUsers.css"
 
 
 type User = {
@@ -12,25 +13,54 @@ type User = {
 }
 
 export const ApiUsers = () => {
-    const [users, setUsers] = useState<User>()
+    const [users, setUsers] = useState<User[]>([])
+    const [searchTerm, setSearchTerm] = useState<string>("")
 
     useEffect(() => {
         const fetchUsers = async () => {
             const response = await fetch("https://jsonplaceholder.typicode.com/users");
             const data = await response.json();
             console.log(data);
+            setUsers(data);
              
         };
         fetchUsers();
     },[]);
+
+    const filterUsers = users.filter(user =>
+        user.name.toLowerCase().includes(searchTerm.toLowerCase())
+        )
 
 
 
   return (
     <div className='users'>
         <h2>ApiUsers</h2>
-        <div className='userCard'>
+        <div className='search'>
+            <input
+                className='input'
+                type='text'
+                placeholder='Search by name'
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
 
+        <ul>
+        {filterUsers.map((user) => (
+          <li key={user.id}>
+            <strong>Name:</strong> {user.name}, <strong>Email:</strong> {user.email}
+          </li>
+        ))}
+      </ul>
+        </div>
+        <div className='userCard'>
+            {
+                users.map((user) =>(
+                    <div key={user.id}>
+                        <p>{user.name}</p>
+                    </div>
+                ))
+            }
         </div>
     </div>
   )
