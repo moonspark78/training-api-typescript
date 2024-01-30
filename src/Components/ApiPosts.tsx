465a6c181040fbc04fa82e019a15194f291cedfd
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-01-30 01:15:17 +0100

    :rocket: :tada: :rocket: console the data  :tada:

--- src/Components/ApiPosts.tsx
+++ src/Components/ApiPosts.tsx
@@ -10,6 +10,18 @@ type Post = {
 
 export const ApiPosts = () => {
     const [posts, setPosts] = useState<Post[]>([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+            const data = await response.json();
+            console.log(data);
+        };
+        fetchData();
+    },[]);
+
+
+
   return (
     <div>ApiPosts</div>
   )
