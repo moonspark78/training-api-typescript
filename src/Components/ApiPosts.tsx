3e9cf12e9345db1ec1a02cc63f63b1d598ed8bcd
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-01-30 01:09:56 +0100

    :rocket: :tada: :rocket: :tada:

--- src/Components/ApiPosts.tsx
+++ src/Components/ApiPosts.tsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+
+type Post = {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
+
 
 export const ApiPosts = () => {
+    const [posts, setPosts] = useState<Post[]>([])
   return (
     <div>ApiPosts</div>
   )
